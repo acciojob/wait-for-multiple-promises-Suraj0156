7c1a7484032d4606ab9c8479fed9bb1b3d7bf41f
Suraj0156 <130788586+Suraj0156@users.noreply.github.com>
2023-09-03 13:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+const promises = [];
+
+for (let i = 0; i < 3; i++) {
+  const delay = Math.random() * 2 + 1;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(delay);
+    }, delay * 1000);
+  });
+
+  promises.push(promise);
+}
+
+Promise.all(promises).then((results) => {
+  const table = document.getElementById("table");
+
+  // Remove the loading text
+  const loadingRow = table.rows[0];
+  loadingRow.remove();
+
+  // Add the results to the table
+  for (let i = 0; i < results.length; i++) {
+    const row = table.insertRow(-1);
+    row.insertCell(0).textContent = `Promise ${i + 1}`;
+    row.insertCell(1).textContent = results[i];
+  }
+
+  // Calculate the total time taken to resolve all promises
+  const totalTime = results.reduce((sum, value) => sum + value, 0);
+
+  // Add a row for the total time
+  const totalRow = table.insertRow(-1);
+  totalRow.insertCell(0).textContent = "Total";
+  totalRow.insertCell(1).textContent = totalTime.toFixed(2);
+});
